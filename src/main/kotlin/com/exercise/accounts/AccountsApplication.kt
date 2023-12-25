15d3fba036c8cd632921c6b87510d4c0ae331ee7
Rifqi Naufal Tohari <rifqinaufaltohari@gmail.com>
2023-12-25 17:48:13 +0700

    add more info into swagger

--- src/main/kotlin/com/exercise/accounts/AccountsApplication.kt
+++ src/main/kotlin/com/exercise/accounts/AccountsApplication.kt
@@ -1,11 +1,32 @@
 package com.exercise.accounts
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+	info = Info(
+		title = "Account microservice REST API Documentation",
+		description = "Account Microservice is a restful HTTP Api service to handle bank Account business logic such as create account, fetch account details, update account, and close account.",
+		version = "v1",
+		contact = Contact(
+			name = "Rifqi Naufal Tohari",
+			email = "rifqinaufaltohari@gmail.com",
+			url = "https://www.linkedin.com/in/rifqi-naufal-tohari/"
+		)
+	),
+	externalDocs = ExternalDocumentation(
+		description =  "Source Code",
+		url = "https://github.com/toharifqi/accounts"
+	)
+)
 class AccountsApplication
 
 fun main(args: Array<String>) {
